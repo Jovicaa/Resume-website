7e0c7a5de745ec761cc32a669421b09fb4504436
Jovica <85282975+Jovicaa@users.noreply.github.com>
2023-05-06 01:09:04 +0100

    Add JS

---
+++ index.js
@@ -0,0 +1,8 @@
+import '../styles/modern-normalize.css';
+import '../styles/styles.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/featured.css'
+import '../styles/components/contact.css'
+import '../styles/util.css';
