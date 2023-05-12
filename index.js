a966118bfed0948d950d7139a26cd4578215d8ea
Jovica <85282975+Jovicaa@users.noreply.github.com>
2023-05-12 17:11:27 +0100

    Update index.js
    Create back to top - button functionallity

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-import '../styles/modern-normalize.css';
-import '../styles/styles.css';
-import '../styles/components/header.css';
-import '../styles/components/hero.css';
-import '../styles/components/about.css';
-import '../styles/components/featured.css'
-import '../styles/components/contact.css'
-import '../styles/util.css';
+    // BACK TO TOP - BTN
+    //_________________________________________________________________________
+
+// Get btn
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;               // For Safari(?)
+  document.documentElement.scrollTop = 0;    // For Chrome, Firefox & Opera
+}
+//________________________________________________________________________________
